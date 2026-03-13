927a452886c6ba506c4e8cbad69f7572b576dcb9
Giga <gigamikasha@gemail.com>
2026-03-13 16:50:20 +0400

    using array in MetroSystem

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
 
-        Speed train = new Speed("model3", 90);
-        System.out.println(train.getTrainModel() + " " + train.getTrainSpeed());
+        Speed speed = new Speed("model3", 90);
+        System.out.println(speed.getTrainModel() + " " + speed.getTrainSpeed());
 
         Driver driver = new Driver();
         driver.setDriverName("Mike");
@@ -16,76 +16,65 @@ public class Main {
         driver2.setDriverName("John");
         System.out.println(driver.getDriverName());
         System.out.println(driver.getDriverId());
+        System.out.println(Driver.getTotalDrivers());
 
         Passenger passenger1 = new Passenger();
         passenger1.setName("Giga");
-
         Passenger passenger2 = new Passenger();
         passenger2.setName("John");
 
         Train train1 = new Train();
         train1.setTrainNumber(101);
         train1.setCapacity(50);
-
         Train train2 = new Train();
         train2.setTrainNumber(102);
         train2.setCapacity(80);
 
-        Ticket ticket = new Ticket();
-        ticket.setPrice(new BigDecimal("2.50"));
-        ticket.setPurchaseTime(LocalDateTime.now());
-        System.out.println(ticket.getPrice());
-        System.out.println(ticket.getPurchaseTime());
-
-        Booking booking = new Booking();
-        booking.bookTicket(passenger1, train1, ticket);
-
-        MetroSystem metro = new MetroSystem();
-
-        metro.setTrain1(train1);
-        metro.setTrain2(train2);
-
-        metro.setPassenger1(passenger1);
-        metro.setPassenger2(passenger2);
-
-        Station station1 = new Station();
-        station1.setLocation("Central Station");
-        metro.setStation1(station1);
-
-        Station station2 = new Station();
-        station2.setLocation("North Station");
-        metro.setStation2(station2);
-
-        Line line1 = new Line();
-        line1.setName("Red Line");
-        metro.setLine1(line1);
-
-        Line line2 = new Line();
-        line2.setName("Blue Line");
-        metro.setLine2(line2);
-
-        System.out.println("Train 1 capacity: " + metro.getTrain1().getCapacity());
-        System.out.println("Train 2 capacity: " + metro.getTrain2().getCapacity());
-        System.out.println("Passenger 1: " + metro.getPassenger1().getName());
-        System.out.println("Passenger 2: " + metro.getPassenger2().getName());
-        System.out.println("Station 1: " + metro.getStation1().getLocation());
-        System.out.println("Station 2: " + metro.getStation2().getLocation());
-        System.out.println(Driver.getTotalDrivers());
-
-        metro.setTicket1(ticket);
+        Ticket ticket1 = new Ticket();
+        ticket1.setPrice(new BigDecimal("2.50"));
+        ticket1.setPurchaseTime(LocalDateTime.now());
         Ticket ticket2 = new Ticket();
         ticket2.setPrice(new BigDecimal("3.50"));
         ticket2.setPurchaseTime(LocalDateTime.now());
-        metro.setTicket2(ticket2);
 
-        System.out.println("Ticket 1 price: " + metro.getTicket1().getPrice());
-        System.out.println("Ticket 2 price: " + metro.getTicket2().getPrice());
+        Booking booking = new Booking();
+        booking.bookTicket(passenger1, train1, ticket1);
+
+        Station station1 = new Station();
+        station1.setLocation("Central Station");
+        Station station2 = new Station();
+        station2.setLocation("North Station");
+
+        Line line1 = new Line();
+        line1.setName("Red Line");
+        Line line2 = new Line();
+        line2.setName("Blue Line");
 
         Schedule schedule = new Schedule();
         schedule.setArrivalTime(LocalDateTime.now());
         schedule.setDepartureTime(LocalDateTime.now().plusMinutes(30));
         schedule.setTrain(train1);
 
+        MetroSystem metro = new MetroSystem();
+        metro.addTrain(train1);
+        metro.addTrain(train2);
+        metro.addPassenger(passenger1);
+        metro.addPassenger(passenger2);
+        metro.addStation(station1);
+        metro.addStation(station2);
+        metro.addLine(line1);
+        metro.addLine(line2);
+        metro.addTicket(ticket1);
+        metro.addTicket(ticket2);
+
+        System.out.println("Train 1 capacity: " + metro.getTrains().get(0).getCapacity());
+        System.out.println("Train 2 capacity: " + metro.getTrains().get(1).getCapacity());
+        System.out.println("Passenger 1: " + metro.getPassengers().get(0).getName());
+        System.out.println("Passenger 2: " + metro.getPassengers().get(1).getName());
+        System.out.println("Station 1: " + metro.getStations().get(0).getLocation());
+        System.out.println("Station 2: " + metro.getStations().get(1).getLocation());
+        System.out.println("Ticket 1 price: " + metro.getTickets().get(0).getPrice());
+        System.out.println("Ticket 2 price: " + metro.getTickets().get(1).getPrice());
         System.out.println("Arrival: " + schedule.getArrivalTime());
         System.out.println("Departure: " + schedule.getDepartureTime());
         System.out.println("Train number: " + schedule.getTrain().getTrainNumber());
--- src/model/MetroSystem.java
+++ src/model/MetroSystem.java
@@ -1,95 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MetroSystem {
 
-    private Train train1;
-    private Train train2;
-    private Station station1;
-    private Station station2;
-    private Passenger passenger1;
-    private Passenger passenger2;
-    private Line line1;
-    private Line line2;
-    private Ticket ticket1;
-    private Ticket ticket2;
+    private List<Train> trains = new ArrayList<>();
+    private List<Station> stations = new ArrayList<>();
+    private List<Passenger> passengers = new ArrayList<>();
+    private List<Line> lines = new ArrayList<>();
+    private List<Ticket> tickets = new ArrayList<>();
 
-    public Train getTrain1() {
-        return train1;
-    }
+    public List<Train> getTrains() { return trains; }
+    public void addTrain(Train train) { trains.add(train); }
 
-    public void setTrain1(Train train1) {
-        this.train1 = train1;
-    }
+    public List<Station> getStations() { return stations; }
+    public void addStation(Station station) { stations.add(station); }
 
-    public Train getTrain2() {
-        return train2;
-    }
+    public List<Passenger> getPassengers() { return passengers; }
+    public void addPassenger(Passenger passenger) { passengers.add(passenger); }
 
-    public void setTrain2(Train train2) {
-        this.train2 = train2;
-    }
+    public List<Line> getLines() { return lines; }
+    public void addLine(Line line) { lines.add(line); }
 
-    public Station getStation1() {
-        return station1;
-    }
-
-    public void setStation1(Station station1) {
-        this.station1 = station1;
-    }
-
-    public Station getStation2() {
-        return station2;
-    }
-
-    public void setStation2(Station station2) {
-        this.station2 = station2;
-    }
-
-    public Passenger getPassenger1() {
-        return passenger1;
-    }
-
-    public void setPassenger1(Passenger passenger1) {
-        this.passenger1 = passenger1;
-    }
-
-    public Passenger getPassenger2() {
-        return passenger2;
-    }
-
-    public void setPassenger2(Passenger passenger2) {
-        this.passenger2 = passenger2;
-    }
-
-    public Line getLine1() {
-        return line1;
-    }
-
-    public void setLine1(Line line1) {
-        this.line1 = line1;
-    }
-
-    public Line getLine2() {
-        return line2;
-    }
-
-    public void setLine2(Line line2) {
-        this.line2 = line2;
-    }
-
-    public Ticket getTicket1() {
-        return ticket1;
-    }
-
-    public void setTicket1(Ticket ticket1) {
-        this.ticket1 = ticket1;
-    }
-
-    public Ticket getTicket2() {
-        return ticket2;
-    }
-
-    public void setTicket2(Ticket ticket2) {
-        this.ticket2 = ticket2;
-    }
+    public List<Ticket> getTickets() { return tickets; }
+    public void addTicket(Ticket ticket) { tickets.add(ticket); }
 }
